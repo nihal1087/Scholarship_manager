ad910085deefc2ff8dd4ea26e19f3093191d8bea
nyl <160739192+nihal1087@users.noreply.github.com>
2026-03-24 21:32:07 +0530

    Initial commit

--- server.js
+++ server.js
@@ -229,4 +229,4 @@ app.delete('/clear-data', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
